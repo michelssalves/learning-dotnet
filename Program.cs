46d36f0c553bc4660c68a96eb6ac2a253cdedd79
Michel <michelssalves@gmail.com>
2024-07-08 11:15:13 -0300

    2024-07-08

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int i = 1;
+
+int j = i++;
+i = j == 42 ? 10 : 20;
+
+Console.WriteLine(j);
+
+Console.WriteLine(i);
+
+Console.Write("Fill your name: ");
+string name =  Console.ReadLine();
+Console.WriteLine($"Hello {name}" );
+
+Console.Write("Fill in your year of birth: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2024 - year;
+
+Console.WriteLine($"You have {age} year old");
+
+if (age > 17) {
+    Console.WriteLine("You are of legal age");
+}else{
+    Console.WriteLine("you are underage");
+} 
+
+string[] names = { "Fredy", "Mariana"};
